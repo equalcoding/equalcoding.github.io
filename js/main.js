e9c53ec94543226feeb8c15fff7732e91753fdb2
equal.coding <info@equalcoding.dev>
2020-05-18 23:59:13 +0100

    disabling cache control

--- js/main.js
+++ js/main.js
@@ -75,6 +75,7 @@ function loadGitRepos() {
     let ele = document.getElementById('github');
     let request = new XMLHttpRequest();
     request.open('GET', `https://api.github.com/users/${gitUsername}/repos?sort=${githubSort}&direction=${githubDirection}`, true);
+    request.setRequestHeader("Cache-Control", "no-cache, no-store, must-revalidate");
     request.onload = function () {
         response = JSON.parse(request.response);
         response.forEach((response, index) => {
@@ -90,7 +91,7 @@ function loadDevToArticles() {
     let ele = document.getElementById('blog');
     let request = new XMLHttpRequest()
     request.open('GET', `https://dev.to/api/articles?username=${devtoUsername}&state=${state}&per_page=${perPage}&page=${page}`, true)
-
+    request.setRequestHeader("Cache-Control", "no-cache, no-store, must-revalidate");
     request.onload = function () {
         response = JSON.parse(request.response);
         response.forEach(response => populateBlogDOM(response, ele));
